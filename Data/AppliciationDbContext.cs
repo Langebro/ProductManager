9044def0ffcff159a1d6e685d5f354dbc189a341
Nicolina Langebro <langebro92_@hotmail.com>
2023-09-17 19:47:29 +0200

    Added DbContect to project

---
+++ Data/AppliciationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using ProductManager.Domain;
+
+namespace ProductManager.Data
+{
+    internal class AppliciationDbContext : DbContext
+    {
+        private string connectionString = "Server=.;Database=ProductManagwer;Integrated Security=true;Encrypt=False";
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder.LogTo(message => Debug.WriteLine(message));
+        }
+
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
